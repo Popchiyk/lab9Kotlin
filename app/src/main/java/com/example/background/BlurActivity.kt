fec5112912773b1c56c3808bdd6b5bc45fea20cc
Popchiyk <roma240103@gmail.com>
2023-04-17 22:29:31 +0300

    Show final output

--- app/src/main/java/com/example/background/BlurActivity.kt
+++ app/src/main/java/com/example/background/BlurActivity.kt
@@ -16,6 +16,7 @@
 
 package com.example.background
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
@@ -34,11 +35,15 @@ class BlurActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBlurBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityBlurBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        viewModel.outputWorkInfos.observe(this, workInfosObserver())
-        binding.goButton.setOnClickListener { viewModel.applyBlur(blurLevel) }
+        // Setup view output image file button
+        binding.seeFileButton.setOnClickListener {
+            viewModel.outputUri?.let { currentUri ->
+                val actionView = Intent(Intent.ACTION_VIEW, currentUri)
+                actionView.resolveActivity(packageManager)?.run {
+                    startActivity(actionView)
+                }
+            }
+        }
     }
 
     private fun workInfosObserver(): Observer<List<WorkInfo>> {
@@ -59,6 +64,16 @@ class BlurActivity : AppCompatActivity() {
 
             if (workInfo.state.isFinished) {
                 showWorkFinished()
+
+                // Normally this processing, which is not directly related to drawing views on
+                // screen would be in the ViewModel. For simplicity we are keeping it here.
+                val outputImageUri = workInfo.outputData.getString(KEY_IMAGE_URI)
+
+                // If there is an output file show "See File" button
+                if (!outputImageUri.isNullOrEmpty()) {
+                    viewModel.setOutputUri(outputImageUri)
+                    binding.seeFileButton.visibility = View.VISIBLE
+                }
             } else {
                 showWorkInProgress()
             }
