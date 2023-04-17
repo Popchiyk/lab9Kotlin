f990348d023738ed04cec6165df789c4546316d2
Popchiyk <roma240103@gmail.com>
2023-04-17 22:25:22 +0300

    Chain your Work

--- app/src/main/java/com/example/background/BlurViewModel.kt
+++ app/src/main/java/com/example/background/BlurViewModel.kt
@@ -27,6 +27,8 @@ import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import com.example.background.workers.BlurWorker
+import com.example.background.workers.CleanupWorker
+import com.example.background.workers.SaveImageToFileWorker
 
 
 class BlurViewModel(application: Application) : ViewModel() {
@@ -34,9 +36,11 @@ class BlurViewModel(application: Application) : ViewModel() {
     internal var imageUri: Uri? = null
     internal var outputUri: Uri? = null
     private val workManager = WorkManager.getInstance(application)
+
     init {
         imageUri = getImageUri(application.applicationContext)
     }
+
     /**
      * Create the WorkRequest to apply the blur and save the resulting image
      * @param blurLevel The amount to blur the image
@@ -81,20 +85,37 @@ class BlurViewModel(application: Application) : ViewModel() {
     }
 
     internal fun applyBlur(blurLevel: Int) {
-        val blurRequest = OneTimeWorkRequestBuilder<BlurWorker>()
+        // Add WorkRequest to Cleanup temporary images
+        var continuation = workManager
+            .beginWith(
+                OneTimeWorkRequest
+                    .from(CleanupWorker::class.java)
+            )
+
+        // Add WorkRequest to blur the image
+        val blurRequest = OneTimeWorkRequest.Builder(BlurWorker::class.java)
             .setInputData(createInputDataForUri())
             .build()
 
-        workManager.enqueue(blurRequest)
-    }
+        continuation = continuation.then(blurRequest)
 
-    class BlurViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
+        // Add WorkRequest to save the image to the filesystem
+        val save = OneTimeWorkRequest.Builder(SaveImageToFileWorker::class.java).build()
 
-        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            return if (modelClass.isAssignableFrom(BlurViewModel::class.java)) {
-                BlurViewModel(application) as T
-            } else {
-                throw IllegalArgumentException("Unknown ViewModel class")
+        continuation = continuation.then(save)
+
+        // Actually start the work
+        continuation.enqueue()
+
+        class BlurViewModelFactory(private val application: Application) :
+            ViewModelProvider.Factory {
+
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return if (modelClass.isAssignableFrom(BlurViewModel::class.java)) {
+                    BlurViewModel(application) as T
+                } else {
+                    throw IllegalArgumentException("Unknown ViewModel class")
+                }
             }
         }
     }
